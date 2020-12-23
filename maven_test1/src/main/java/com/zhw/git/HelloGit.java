ccfcfd7ffccacce64b44a476c82472f937985e5d
zhw <2462517308@qq.com>
2020-12-23 11:10:45 +0800

    第一次添加文件

---
+++ maven_test1/src/main/java/com/zhw/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.zhw.git;
+
+/**
+ * @author shkstart
+ * @create 2020-12-23 11:03
+ */
+public class HelloGit {
+
+
+    public static void main(String[] args) {
+        System.out.println("hellogit");
+    }
+
+}
